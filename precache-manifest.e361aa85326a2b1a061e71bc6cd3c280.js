ad96c40ea49b873627a20e0d631212af6173b57a
Charlotte Curran <charlottemariecurran@gmail.com>
2020-01-06 22:03:52 +0100

    Updates

---
+++ precache-manifest.e361aa85326a2b1a061e71bc6cd3c280.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc5811a46b2e8a7f031cb602ae2d85fe",
+    "url": "/salt-study/index.html"
+  },
+  {
+    "revision": "126ff24e6a8e20984c13",
+    "url": "/salt-study/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9b80394a4401d7b3aeab",
+    "url": "/salt-study/static/js/2.ea4b4a81.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/salt-study/static/js/2.ea4b4a81.chunk.js.LICENSE"
+  },
+  {
+    "revision": "126ff24e6a8e20984c13",
+    "url": "/salt-study/static/js/main.7a4ff491.chunk.js"
+  },
+  {
+    "revision": "10036dfa3b820c830f40",
+    "url": "/salt-study/static/js/runtime-main.e687f648.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/salt-study/static/media/logo.5d5d9eef.svg"
+  }
+]);
